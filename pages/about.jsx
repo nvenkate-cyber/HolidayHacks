e79e89c6bc7103237a6753468c789a0a273ba8a9
nvenkate-cyber <68961147+nvenkate-cyber@users.noreply.github.com>
2020-12-24 17:57:36 -0500

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -9,18 +9,17 @@ function About() {
 			  <title>About Us</title>
 			</Head>
 
-			<div style = {{ backgroundImage:'url("https://cdn.pixabay.com/photo/2017/10/26/19/45/christmas-2892235__340.png")', backgroundsize: 'cover'}}>
+			<div style = {{ backgroundImage:'url("https://www.sapporo.co.uk/wp-content/uploads/2018/08/Christmas-decorations-iStock_000048591732_Small.jpg")', backgroundSize: 'cover'}}>
 				<Navbar />
 
 				<section className="hero">
 					<div className="container">
 						<div className="text-wrapper w-full">
 						<h1 className="title" style ={{color: 'white'}}>
-							<br></br>
-							<br></br>
-							<br></br>About Us<br></br><br></br></h1>
+							About Us<br></br></h1>
 						<p className="description" style ={{color: 'white'}}>
 						Christmas is one of the most hectic holidays of the year. We are here to make things easier for you! 
+						<br></br><br></br><br></br><br></br><br></br><br></br>
 						<br></br><br></br><br></br><br></br><br></br><br></br></p>
 						</div>
 					</div>
